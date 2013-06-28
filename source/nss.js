34d048ccc4be7f54c2a5f96797ce72052d2b6c47
mohs3n <mohsenniyazdel@gmail.com>
2013-06-28 18:26:11 +0430

    Append Tag

--- source/nss.js
+++ source/nss.js
@@ -1,11 +1,24 @@
 (function( $ ) {
     $.fn.nss = function( ) {
+    	function appendtag(){
+    		$('body').append('
+    			<div class="shower">
+    			<div class="screen">
+    			<div class="closebtn"></div>
+    			<div class="backbtn"></div>
+    			<div class="forwardbtn"></div>
+    			</div>
+    			</div>
+    		');
+    	};
         var elems = this;
         return elems.on('click', function(e) {
             e.preventDefault();
             var thisindx = elems.index(this);
             console.log(thisindx);
             console.log(elems.length);
+            appendtag();
         });
+
     };
 }( jQuery ))
