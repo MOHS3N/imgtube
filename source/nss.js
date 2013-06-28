da585553d514aebee9d122b9404bb354824db274
mohs3n <mohsenniyazdel@gmail.com>
2013-06-28 19:18:58 +0430

    create appenttag() Function.

--- source/nss.js
+++ source/nss.js
@@ -1,23 +1,37 @@
 (function( $ ) {
+
+	function appendtag(tag,len,active){
+       $('body').append('<div class="shower"></div>');
+       $('div.shower').append('<div class="screen"></div>');
+       $('div.screen').append('<div class="tube"></div>');
+       for(var num=0;num<len;num++){
+       	$('div.tube').append('<img src="' + tag.eq(num).attr('href')+'" width="950" height="600"/>');
+       };
+
+       for(var i=0; i<active;i++){
+			$('div.tube > img').eq(i).attr('class','leftout');
+			$('div.tube > img').eq(i).attr('style','left : -960px;')
+		};
+
+		for(var j=active+1;j<len;j++){
+			$('div.tube > img').eq(j).attr('class','rightout');
+			$('div.tube > img').eq(j).attr('style','right : -960px;')
+		};
+
+		$('div.tube > img').eq(active).attr('class','active');
+		$('div.tube > img').eq(active).attr('style','left : 0px; right : 0px;');
+     }
+
     $.fn.nss = function( ) {
-    	function appendtag(){
-    		$('body').append('
-    			<div class="shower">
-    			<div class="screen">
-    			<div class="closebtn"></div>
-    			<div class="backbtn"></div>
-    			<div class="forwardbtn"></div>
-    			</div>
-    			</div>
-    		');
-    	};
+    	
         var elems = this;
         return elems.on('click', function(e) {
             e.preventDefault();
             var thisindx = elems.index(this);
+            var elemslen = elems.length;
             console.log(thisindx);
             console.log(elems.length);
-            appendtag();
+            appendtag(elems,elemslen,thisindx);
         });
 
     };
